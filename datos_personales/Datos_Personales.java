a6709c470ef099c5edfb49ccc609cf813459c57f
Apolo2081 <00085617@uca.edu.sv>
2018-03-14 22:42:51 -0600

    Datos_Personales
    Crear un programa java utilizando Netbeans que imprima en consola todos sus datos generales.

---
+++ datos_personales/Datos_Personales.java
@@ -0,0 +1,22 @@
+package datos_personales;
+/*
+ * @author Manuel Ernesto Cardona Chicas
+ */
+public class Datos_Personales {
+
+    public static void main(String[] args) {     
+        System.out.println("\tDATOS GENERALES DE ESTUDIANTE"); 
+        System.out.println("-------------------------------------------"); 
+        System.out.println(" Nombres:  Manuel Ernesto.");
+        System.out.println(" Apellidos: Cardona Chicas.");
+        System.out.println(" Edad: 20 a\u00f1os.");
+        System.out.println(" Fecha de nacimiento: 04 de Abril de 1998");
+        System.out.println(" Sexo: Masculino.");         
+        System.out.println(" Nacionalidad: Salvadore\u00f1o.");   
+        System.out.println(" Ocupacion: Estudiante.");         
+        System.out.println(" Telefono: 7777-7778"); 
+        System.out.println("-------------------------------------------");
+    
+    }
+    
+}
